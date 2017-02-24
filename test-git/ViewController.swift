66aeb46b0373787c627067c3d84e037d65ece98c
LoveTheTaMa <97174547@qq.com>
2017-02-24 16:10:22 +0800

    创建项目

---
+++ test-git/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test-git
+//
+//  Created by 陈洪志 on 17/2/16.
+//  Copyright © 2017年 陈洪志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
